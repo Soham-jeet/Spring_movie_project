f3778dcd507224eeaee541377fd1d15adb4f8850
Soham <sohamdas273@gmail.com>
2022-06-28 12:01:30 +0530

    First Commit

---
+++ src/main/java/com/practice/DemoApplication.java
@@ -0,0 +1,46 @@
+package com.practice;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import com.practice.DTO.DirectorDTO;
+import com.practice.DTO.MovieDTO;
+import com.practice.Service.MovieService;
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		AbstractApplicationContext context = (AbstractApplicationContext)SpringApplication.run(DemoApplication.class, args);
+	    
+		
+		MovieService service = (MovieService)context.getBean("movieservice");
+		//Insert 
+		MovieDTO dto = new MovieDTO("BNC",LocalDate.now(),LocalDateTime.now());
+		List<DirectorDTO> directorList = Arrays.asList(
+				new DirectorDTO("dse", "hgr", "West Bengal","dse@outlook.com",9008274236L),
+				new DirectorDTO("abc","wyz","Mysore","john@infosys.com",9412341736L)
+				);
+        service.searchBasedOnTitle("ABC");
+        //Get all names of Director from movie title
+        System.out.println("GET ALL DIRECTORS FROM MOVIE TITLE");
+        service.getDirectorListFromTitle("ABC");
+        //Get all Movies from Director Name
+        System.out.println("GET ALL MOVIES FROM DIRECTOR NAME");
+        service.getMovieListFromDirectorName("abc");
+        //Get All movies
+        System.out.println(service.displayAll().toString());       
+	}
+
+}
